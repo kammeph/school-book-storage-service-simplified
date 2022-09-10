2f1ea30e1b916e0701a3692686c8a7966b290063
kammeph <ph.kammerer@googlemail.com>
2022-09-11 00:43:56 +0200

    feat: added database initialization

---
+++ db/sql.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	dbdriver   = os.Getenv("DB_DRIVER")
+	dbuser     = os.Getenv("DB_USER")
+	dbpassword = os.Getenv("DB_PASSWORD")
+	dbhost     = os.Getenv("DB_HOST")
+	dbport     = os.Getenv("DB_PORT")
+	dbdbname   = os.Getenv("DB_DATABASE")
+)
+
+func NewSqlDB() *sql.DB {
+	connStr := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbuser, dbpassword, dbhost, dbport, dbdbname)
+	db, err := sql.Open(dbdriver, connStr)
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	log.Println("Successfully connected and pinged to sql database.")
+	return db
+}
