d1e7860e483fffd2b3296c77211da593d373c53a
kammeph <ph.kammerer@googlemail.com>
2022-09-11 00:49:58 +0200

    feat: implemented http server

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	"github.com/kammeph/school-book-storage-service-simplified/auth"
+	"github.com/kammeph/school-book-storage-service-simplified/db"
+	"github.com/kammeph/school-book-storage-service-simplified/users"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	db := db.NewSqlDB()
+	defer db.Close()
+	auth.AddAuthController(db)
+	users.AddUsersController(db)
+	repo := users.NewSqlUserRepository(db)
+	http.HandleFunc("/api/users/update", func(w http.ResponseWriter, r *http.Request) {
+		var user users.UserDto
+		json.NewDecoder(r.Body).Decode(&user)
+		err := repo.UpdateUser(context.Background(), user)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+	})
+	http.ListenAndServe(":9090", nil)
 }
